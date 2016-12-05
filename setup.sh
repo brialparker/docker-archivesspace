ccdff8d34d912dd397569fb24145258ebe5f1862
Bria Parker <blparker@umd.edu>
2016-12-05 13:21:02 -0500

    path changes part 2

--- setup.sh
+++ setup.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-cat /dev/null > /config/config.rb
+cat /dev/null > /archivesspace/config/archivesspace/config-defaults.rb
 
-echo "AppConfig[:search_user_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\"" >> /config/config.rb
-echo "AppConfig[:public_user_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\""  >> /config/config.rb
-echo "AppConfig[:staff_user_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;") "\""  >> /config/config.rb
-echo "AppConfig[:frontend_cookie_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\""  >> /config/config.rb
-echo "AppConfig[:public_cookie_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\"" >> /config/config.rb
+echo "AppConfig[:search_user_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\"" >> /archivesspace/config/archivesspace/config-defaults.rb
+echo "AppConfig[:public_user_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\""  >> /archivesspace/config/archivesspace/config-defaults.rb
+echo "AppConfig[:staff_user_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;") "\""  >> /archivesspace/config/archivesspace/config-defaults.rb
+echo "AppConfig[:frontend_cookie_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\""  >> /archivesspace/config/archivesspace/config-defaults.rb
+echo "AppConfig[:public_cookie_secret] = \""$(ruby  -e "require 'securerandom'; print SecureRandom.hex;")"\"" >> /archivesspace/config/archivesspace/config-defaults.rb
 
 
 if [[ "$ARCHIVESSPACE_DB_TYPE" == "mysql" ]]; then
   echo "AppConfig[:db_url] = 'jdbc:mysql://$DB_PORT_3306_TCP_ADDR:3306/$ARCHIVESSPACE_DB_NAME?user=$ARCHIVESSPACE_DB_USER&password=$ARCHIVESSPACE_DB_PASS&useUnicode=true&characterEncoding=UTF-8'" \
-    >> /config/config.rb
+    >> /archivesspace/config/archivesspace/config-defaults.rb
   /scripts/setup-database.sh
 fi
 
 
 if [[ $FRONTEND_PROXY_URL ]]; then
-  echo "AppConfig[:frontend_proxy_url] = \"$FRONTEND_PROXY_URL\"" >> /config/config.rb 
+  echo "AppConfig[:frontend_proxy_url] = \"$FRONTEND_PROXY_URL\"" >> /archivesspace/config/archivesspace/config-defaults.rb 
 fi
 
 if [[ $FRONTEND_PREFIX ]]; then
-  echo "AppConfig[:frontend_prefix] = \"$FRONTEND_PREFIX\"" >> /config/config.rb 
+  echo "AppConfig[:frontend_prefix] = \"$FRONTEND_PREFIX\"" >> /archivesspace/config/archivesspace/config-defaults.rb 
 fi
 
 if [[ $PUBLIC_PROXY_URL ]]; then
-  echo "AppConfig[:public_proxy_url] = \"$PUBLIC_PROXY_URL\"" >> /config/config.rb 
+  echo "AppConfig[:public_proxy_url] = \"$PUBLIC_PROXY_URL\"" >> /archivesspace/config/archivesspace/config-defaults.rb 
 fi
 
 if [[ $PUBLIC_PREFIX ]]; then
-  echo "AppConfig[:public_prefix] = \"$PUBLIC_PREFIX\"" >> /config/config.rb 
+  echo "AppConfig[:public_prefix] = \"$PUBLIC_PREFIX\"" >> /archivesspace/config/archivesspace/config-defaults.rb 
 fi
 
 for PLUGIN in /archivesspace/plugins/*; do
-  [[ -d $PLUGIN ]] && echo "AppConfig[:plugins] << '${PLUGIN##*/}'" >> /config/config.rb
+  [[ -d $PLUGIN ]] && echo "AppConfig[:plugins] << '${PLUGIN##*/}'" >> /archivesspace/config/archivesspace/config-defaults.rb
 done
 
-exec /archivesspace.sh
+exec /archivesspace/archivesspace.sh
